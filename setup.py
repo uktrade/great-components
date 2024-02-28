0103f7c6c08d9f354068c43dfed8fa262f789f11
bobby-didcoding <bobby@didcoding.com>
2024-02-28 12:56:31 +0000

    Removed meta blocks from base template

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="great_components",
-    version="2.6.2",
+    version="2.6.3",
     url="https://github.com/uktrade/great-components",
     license="MIT",
     author="DIT",
